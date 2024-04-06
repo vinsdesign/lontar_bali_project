6f830a821c06a2dea6dc94c5bb5e9b117c82caad
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-06 12:52:42 +0800

    Install Fonts Awesome & depedency

--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faHouse)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
 <template>
-  <main class="text-3xl bg-slate-600 flex justify-center items-center">Hello World</main>
+  <main class="text-3xl bg-slate-600 flex justify-center items-center">
+    <font-awesome-icon icon="fa-solid fa-house" />Hello World
+  </main>
 </template>
