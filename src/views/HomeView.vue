a693d7b8d0c7ece30c8f393ffd66c7914ec7d43c
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-08 12:23:40 +0800

    install daisyUI and configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <main class="text-3xl flex justify-center items-center">
+  <main class="text-3xl flex justify-center items-center font-montserrat">
     <font-awesome-icon icon="fa-solid fa-house" />Hello World
   </main>
 </template>
