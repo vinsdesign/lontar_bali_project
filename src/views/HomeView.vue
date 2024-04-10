6e10ee0a7502a3c70a4e52debc29c9459b3832b9
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-10 11:15:00 +0800

    feat: FooterComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,15 @@
 import NavbarComponent from '@/components/navbar/NavbarComponent.vue'
 import CarouselImage from '@/components/carousel/CarouselImage.vue'
 import SearchComponent from '@/components/SearchComponent/SearchComponent.vue'
+import FooterComponent from '@/components/footer/FooterComponent.vue'
 </script>
 
 <template>
-<NavbarComponent />
+  <NavbarComponent />
   <header class="relative">
     <CarouselImage />
     <SearchComponent />
   </header>
+  <main></main>
+  <FooterComponent />
 </template>
