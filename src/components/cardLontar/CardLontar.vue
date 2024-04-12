62941e9af618f0707489311bc60c21ab93ef3372
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-12 10:52:00 +0800

    Feat: CardComponent

---
+++ src/components/cardLontar/CardLontar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center items-center">
+      <div
+        class="card card-side w-[1000px] h-[180px] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
+      >
+        <div class="card-body">
+          <h2 class="card-title font-montsBold">Aji Kreket</h2>
+          <h2 class="font-montsMedium">I Dewa Made Darmawan | 1991 Masehi | Gianyar</h2>
+          <p class="font-montserrat">
+            Detail Deskripsi Lengkap Lontar, judul lontar : Aji Kreket, tipe bahan: -, subjek,
+            klasifikasi:- , bahasa:-, koleksi:- , Penulis:
+            <a href="" class="text-mediumBlue font-montsMedium">Selengkapnya..</a>
+          </p>
+        </div>
+        <figure>
+          <img class="w-96" src="../../assets/Tulis_Lontar.jpg" alt="Lontar" />
+        </figure>
+      </div>
+    </div>
+  </template>
+  
---
+++ src/components/mainContent/CardPencarianLontar.vue
@@ -0,0 +1,8 @@
+<script setup>
+import CardLontar from '@/components/cardLontar/CardLontar.vue'
+</script>
+<template>
+  <div v-for="item in items" :key="item">
+    <CardLontar />
+  </div>
+</template>
